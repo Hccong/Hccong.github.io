2c823c4eb93c43b448d2fb39df2e8dbf274b54a0
hansuo <hccong_go@163.com>
2017-05-19 17:56:07 +0800

    no message

--- vueblog/src/AppCenter.vue
+++ vueblog/src/AppCenter.vue
@@ -1,39 +1,101 @@
 <template>
 		<div class='app_center'>
-      <div class='center_banner'>
-          <img src="https://ss0.bdstatic.com/94oJfD_bAAcT8t7mm9GUKT-xh_/timg?image&quality=100&size=b4000_4000&sec=1495103770&di=2080756a519276466fde89c1bc70cedc&src=http://img.zcool.cn/community/0155fd580d9db9a84a0e282b276e3c.jpg@900w_1l_2o_100sh.jpg"> 
-      </div>
-      <div class='center_ad'>
+        <div class='center_banner'>
+            <img src="https://ss0.bdstatic.com/94oJfD_bAAcT8t7mm9GUKT-xh_/timg?image&quality=100&size=b4000_4000&sec=1495103770&di=2080756a519276466fde89c1bc70cedc&src=http://img.zcool.cn/community/0155fd580d9db9a84a0e282b276e3c.jpg@900w_1l_2o_100sh.jpg"> 
+        </div>
+        <div class='center_ad'>
+             <div>
+                 <transition enter-active-class="zoomInLeft" leave-active-class="zoomOutRight">
+                    <p v-show="shows" class="animated rubberBand 1s infinite" >2017年新的起点,技术研究院正式开通啦~</p>
+                 </transition>
+             </div>
+        </div>
+        <div class='center_table'>
+     <!--      <Row type="flex" justify="space-around" class="code-row-bg" v-for='(index, value) in arr'>
+            <Col span="1" offset="0"><div>{{value}}</div></Col>
+            <div class='clear'></div>
+          </Row> -->
 
-      </div>
+
+            <div class="row" v-for='(index, value) in arr'>
+              <div class="col-md-<span style="color: #fff;</div>
+            </div>
+
+        </div>
     </div>
 </template>
 
 
-<script type="text/javascript">
+<script>
 	export default({
-		data: {
-
+		data() {
+        return{
+          shows: true,
+          arr: ['1', '2', '3', '4', '5','11', '22', '33', '44', '55']
+        }
 		},
+    mounted: function () {
+       
+    },
 		methods: {
-
-		}
-	});
+     
+		},
+    transitions: {
+      bounce: {
+        enterClass: 'bounceInLeft',
+        leaveClass: 'bounceOutRight'
+      }
+    },
+    watch: {
+    
+    }
+});
 </script>
 
 <style type="text/css">
 .app_center {
   background-color: #f5f5f5;
-  height: 700px;
+  height: 1700px;
 }
 .center_banner img {
   padding: 0;
   margin: 0;
+  margin: 0 10%;
+  width: 80%;
+  display: block;
 }
 .center_ad {
-  margin-top: 0;
-  padding-top: 0;
+  margin: 0 10%;
   background-color: #e8e8e8;
   height: 40px;
+  text-align: left;
 }
+.center_ad p {
+  line-height: 40px;
+  text-align: center;
+  font-size: 15px;
+}
+.center_table {
+  margin-top: 30px;
+  text-align: center;
+}
+.code-row-bg {
+  background-color: red;
+}
+.center_table div {
+  display: inline-block;
+  background-color: #fff;
+  height: 205px;
+  width: 290px;
+  margin-top: 30px;
+  margin-right: 20px;
+  border: 1px solid #000;
+  border-radius: 5px;
+}
+.clear {
+  clear: both;
+}
+
 </style>
+
+
--- vueblog/src/AppTop.vue
+++ vueblog/src/AppTop.vue
@@ -1,17 +1,16 @@
 <template>
 		<div class='app_top'>
+          <img class='logo_img' src="./assets/logo.png">
           <ul class='top_ul'>
-            <!-- <li class='li_img'><img src="./assets/logo.png"></li> -->
-            <li>Home</li>
-            <li>IOS</li>
-            <li>Android</li>
-            <li>Html</li>
-            <li>Java</li>
+            <li><a href="javascript:void(0);">HOME</a></li>
+            <li><a href="javascript:void(0);">IOS</a></li>
+            <li><a href="javascript:void(0);">Android</a></li>
+            <li><a href="javascript:void(0);">HTML</a></li>
+            <li><a href="javascript:void(0);">JAVA</a></li>
           </ul>
           <Input class='top_search' v-model="value" placeholder="请搜索..." style="width: 200px;" ></Input>
-      </div>
+    </div>
 </template>
-
 
 <script type="text/javascript">
 	export default({
@@ -29,23 +28,40 @@
   height: 70px;
   line-height: 70px;
 }
-.top_ul {
-  text-align: center;
-}
-.top_ul li{
-  list-style: none;
+.logo_img {
   float: left;
-  margin: 0 1.2%;
-  height: 70px;
-  line-height: 70px;
-  padding: 3px 3px;
-  background-color: red;
+  margin-top: 10px;
+  margin-left: 5%;
+  margin-right: 10%;
+}
+.top_ul {
+  list-style:none; 
+}
+
+.top_ul li {
+  float: left;
+}
+
+.top_ul li a {
+    color: rgb(51, 51, 51); 
+    line-height: 30px; 
+    padding: 4px 20px;
+    text-decoration: none;
+    white-space: nowrap;
+    margin-top: 15px;
+}
+.top_ul li a:hover {
+    background-color: #555; 
+    border: 0.1px #555 solid; 
+    border-radius: 3px;
+    color: #fff;
 }
 .li_img {
    padding-left: 10%;
 }
 .top_search {
- 
+ float: right;
+ right: 5%;
 }
 .top_ul li:hover{
   color: red;
